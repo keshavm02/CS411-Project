dc537b85745dbe72fdbc991b573151b4f90a438a
Keshav Maheshwari <km02@bu.edu>
2020-12-14 18:50:10 -0500

    added fetch code and redirect to backend button

--- prototype/routes/googleOauth.js
+++ prototype/routes/googleOauth.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.route("/").get(async (req, res, next) => {
-    res.render("signin");
+
+    // res.render("signin");
+    var url = 'localhost:8000/login/google-oauth2/';
+    var data = {username: 'unknown'};
+    fetch(url, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
 });
 
 module.exports = router;
