bb07560435872d2cd1027203f639e74c530a3880
tsmorita3 <tsubasamorita3@gmail.com>
2020-12-14 16:42:33 -0500

    finished up userInfo page

--- prototype/routes/userInfo.js
+++ prototype/routes/userInfo.js
@@ -1,12 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.route('/')
-    .get(async (req, res, next) => {
-        res.render('index');
-    })
-    .post(async (req, res, next) => {
-
-    })
+router.route("/").get(async (req, res, next) => {
+  res.render("userInfo");
+});
+//   .post(async (req, res, next) => {});
 
 module.exports = router;
